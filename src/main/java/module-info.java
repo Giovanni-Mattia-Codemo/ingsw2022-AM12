96c96ca09c17e601232bf33b9377834f9d21e59b
Vale <valerio.c998@gmail.com>
2022-03-27 22:19:04 +0100

    updated the pom with the recommendation for M1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,7 @@ module it.polimi.ingsw2022am12 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
+
 
     opens it.polimi.ingsw2022am12.JavaFX to javafx.fxml;
     exports it.polimi.ingsw2022am12.JavaFX;
