6f1f3ef04ea546eec53d82d3f064f40d02adf44f
Giovanni Mattia Codemo <giovannimattia.codemo@mail.polimi.it>
2022-05-09 21:13:04 +0200

    Fixed module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module it.polimi.ingsw2022am12 {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
 
     exports it.polimi.ingsw2022am12.server.model;
     exports it.polimi.ingsw2022am12.server.controller;
