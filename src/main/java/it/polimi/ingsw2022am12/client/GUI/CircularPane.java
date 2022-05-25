ff59cac6dd73fd1298c3191db08e406542d425c3
Giovanni Mattia Codemo <giovannimattia.codemo@mail.polimi.it>
2022-05-25 21:01:46 +0200

    Changes by Valerio Capo

--- src/main/java/it/polimi/ingsw2022am12/client/GUI/CircularPane.java
+++ src/main/java/it/polimi/ingsw2022am12/client/GUI/CircularPane.java
@@ -10,12 +10,27 @@ public class CircularPane extends Pane {
     @Override
     protected void layoutChildren() {
 
-        final int radiusOfLayout = 200;
+        final double radiusOfLayout = Math.min(maxHeightProperty().getValue()/2, maxWidthProperty().getValue()/2);
         final double offsetForChildrenInCircle = 360 / getChildren().size();
         double degrees = 0;  //Starting point from 0 degrees, moves clockwise from now on
+        double x = 0;
+        double y = 0;
         for (Node node : getChildren()) {
-            double x = radiusOfLayout * Math.cos(Math.toRadians(degrees)) + getWidth() / 2;
-            double y = radiusOfLayout * Math.sin(Math.toRadians(degrees)) + getHeight() / 2;
+            if(360-Math.toDegrees(Math.atan(maxHeightProperty().getValue()/maxWidthProperty().getValue()))<=degrees||degrees<Math.toDegrees(Math.atan(maxHeightProperty().getValue()/maxWidthProperty().getValue()))){
+                x = maxWidthProperty().getValue()*0.9;
+                y = (x-maxWidthProperty().getValue()*0.5)*Math.tan(Math.toRadians(degrees))+maxHeightProperty().getValue()*0.5;
+            }else if(180-Math.toDegrees(Math.atan(maxHeightProperty().getValue()/maxWidthProperty().getValue()))>degrees&&degrees>=Math.toDegrees(Math.atan(maxHeightProperty().getValue()/maxWidthProperty().getValue()))){
+                y= maxHeightProperty().getValue()*0.9;
+                x = (y-maxHeightProperty().getValue()*0.5)/Math.tan(Math.toRadians(degrees))+maxWidthProperty().getValue()*0.5;
+            }else if((Math.toRadians(180)-Math.atan(maxHeightProperty().getValue()/maxWidthProperty().getValue())<=Math.toRadians(degrees)&&Math.toRadians(degrees)<Math.toRadians(180)+Math.atan(maxHeightProperty().getValue()/maxWidthProperty().getValue()))){
+                x = maxWidthProperty().getValue()*0.1;
+                y = (maxWidthProperty().getValue()*0.5-x)*Math.tan(Math.toRadians(degrees))+maxHeightProperty().getValue()*0.5;
+            }else if(Math.toRadians(180)+Math.atan(maxHeightProperty().getValue()/maxWidthProperty().getValue())<=Math.toRadians(degrees)&&Math.toRadians(degrees)<Math.toRadians(360)-Math.atan(maxHeightProperty().getValue()/maxWidthProperty().getValue())){
+                y= maxHeightProperty().getValue()*0.1;
+                x = (maxHeightProperty().getValue()*0.5-y)/Math.tan(Math.toRadians(degrees))+maxWidthProperty().getValue()*0.5;
+            }
+            //double x = Math.min(Math.abs()) radiusOfLayout * Math.cos(Math.toRadians(degrees)) + getWidth()/ 2;
+            //double y = radiusOfLayout * Math.sin(Math.toRadians(degrees)) + getHeight() / 2;
             layoutInArea(node, x - node.getBoundsInLocal().getWidth() / 2, y - node.getBoundsInLocal().getHeight() / 2, getWidth(), getHeight(), 0.0, HPos.LEFT, VPos.TOP);
             degrees += offsetForChildrenInCircle;
         }
